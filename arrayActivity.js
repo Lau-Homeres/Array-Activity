04e70bcdfef35e3fe5e83069aff9c1737908b211
Lau Homeres <laurencehomeres0@gmail.com>
2024-10-18 14:29:54 +0800

    Initial commit

---
+++ arrayActivity.js
@@ -0,0 +1,12 @@
+const favSingers = ["Beyonce", "Bruno Mars", "Megan Trainor"];
+
+console.log("first singer: ", favSingers[0]);
+
+const favNumbers = [7, 11, 22, 5];
+
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log("First item in mixedArr: ", mixedArr[0]);  
+console.log("Second item in mixedArr: ", mixedArr[1]); 
+console.log("Third item in mixedArr: ", mixedArr[2]); 
+console.log("Fourth item in mixedArr: ", mixedArr[3]);
